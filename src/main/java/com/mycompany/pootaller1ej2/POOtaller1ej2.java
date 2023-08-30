a9fcd25bf96ddd611e9e8854715f159213ca4546
ASUS <ASUS@DESKTOP-L6MVLAR>
2023-08-30 11:39:36 -0500

    En este ejercicio nos dan un número, y elaboramos un algoritmo que lea el numero, y obtenga su cuadrado y su cubo

---
+++ src/main/java/com/mycompany/pootaller1ej2/POOtaller1ej2.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.pootaller1ej2;
+//Este codigo lee un número dado y devuelve su cuadrado y su cubo
+
+import java.util.Scanner;
+
+public class POOtaller1ej2 {
+
+    public static void main(String[] args) {
+        float x;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese el valor de x:");
+        x = entrada.nextFloat();
+        System.out.println("El cuadrado de x es: " + Math.pow(x, 2));
+        System.out.println("El cubo de x es: " + Math.pow(x, 3));
+        
+        
+       
+    }
+}
